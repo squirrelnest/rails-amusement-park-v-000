66bd52e0d4cd37b3ca8fce9a4ffcc25e778f5d45
mikka <mikkanthrope@gmail.com>
2017-10-13 22:05:35 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -30,6 +30,9 @@ class AttractionsController < ApplicationController
   end
 
   def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
   end
 
   private
